abefbe05fed22da6a6c476b3254a266750b443fa
honohaga2002-afk <hono.haga2002@gmail.com>
2026-08-29 12:18:36 +0900

    Update google-auth.js

--- api/google-auth.js
+++ api/google-auth.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
-      'https://toma-share-cloud-live.vercel.app/api/google/callback'
+      'https://toma-share-cloud-live.vercel.app/api/google-callback'
     );
 
     const url = oauth2Client.generateAuthUrl({
